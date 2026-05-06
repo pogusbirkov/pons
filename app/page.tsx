ef110acc5f35a0061bf3620fc157669d4b8702a6
pogusbirkov <pogusbirkovpb@gmail.com>
2026-05-06 16:48:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const tabs = Array.from({ length: 10 }, (_, i) => ({
     id: i + 1,
     title: `Вкладка ${i + 1}`,
-    content: `Здесь будет ваш текст для раздела №${i + 1}. Ппавыфая лох позже.`
+    content: `Cлавик №${i + 1}. лох позже.`
   }));
 
   const [activeTab, setActiveTab] = useState(tabs[0]);
